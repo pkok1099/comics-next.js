4c360648e45e219b571a8c78e4395b92e7f127ac
OnLasdan <blackdm1024@gmail.com>
2024-12-01 14:00:31 +0700

    apalah

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,26 +11,6 @@ module.exports = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
-      animation: {
-        particleToCard:
-          'particleToCard 1000ms ease-out', // Menambahkan animasi particleToCard
-      },
-      keyframes: {
-        particleToCard: {
-          '0%': {
-            transform: 'scale(0.1)',
-            opacity: '0',
-          },
-          '50%': {
-            transform: 'scale(1.1)',
-            opacity: '0.5',
-          },
-          '100%': {
-            transform: 'scale(1)',
-            opacity: '1',
-          },
-        },
-      },
     },
   },
   plugins: [],
