e278824c0307c23d80a59ed4aa77c58d2f86525e
.M.U.F.A.R. <132034817+Pkok1024@users.noreply.github.com>
2024-12-20 20:24:22 +0000

    `Removed proxy API handler`

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,21 +1,25 @@
-export default async function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { url, head } = req.query; // Ambil URL dan parameter `head`
 
   if (!url) {
     return res.status(400).json({ message: 'URL is required' });
   }
 
-  let selectedHeaders = {};
+  let selectedHeaders: Record<string, string> = {};
 
   try {
     switch (head) {
       case 'komikindo': // Pilih header untuk another
-        selectedHeaders = (await import(`@/f/commonHeaders`)).default || {};
+        selectedHeaders = (await import('@/f/commonHeaders')).default || {};
         break;
 
       default: // Gunakan header default jika `head` tidak valid
         if (!head) {
-          selectedHeaders = (await import(`@/f/doujinHeaders`)).default || {};
+          selectedHeaders = (await import('@/f/doujinHeaders')).default || {};
         } else {
           return res.status(400).json({
             message: `Invalid head parameter: ${head}`,
@@ -24,14 +28,14 @@ export default async function handler(req, res) {
         break;
     }
   } catch (error) {
-    console.error(`Failed to load headers for head=${head}:`, error.message);
+    console.error(`Failed to load headers for head=${head}:`, (error as any).message);
     return res.status(500).json({
       message: `Failed to load headers for head=${head}`,
     });
   }
 
   try {
-    const response = await fetch(decodeURIComponent(url), {
+    const response = await fetch(decodeURIComponent(url as string), {
       method: 'GET',
       headers: {
         ...selectedHeaders, // Gabungkan header yang dipilih
@@ -46,11 +50,15 @@ export default async function handler(req, res) {
     const contentType = response.headers.get('content-type'); // Dapatkan tipe konten
     const imageBuffer = await response.arrayBuffer(); // Baca buffer gambar
 
-    res.setHeader('Content-Type', contentType); // Set tipe konten pada respon
+    if (contentType) {
+        res.setHeader('Content-Type', contentType); // Set tipe konten pada respon
+    } else {
+        res.setHeader('Content-Type', 'application/octet-stream'); // Fallback content type
+    }
     res.setHeader('Cache-Control', 'public, max-age=86400'); // Cache selama 1 hari
     res.send(Buffer.from(imageBuffer)); // Kirim gambar ke klien
   } catch (error) {
-    console.error('Error in proxy:', error.message);
+    console.error('Error in proxy:', (error as any).message);
     res.status(500).json({ message: 'Failed to proxy image' });
   }
 }
