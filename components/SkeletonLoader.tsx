26f9a268594a059e5e568ab5d43cf2d67fe8609d
OnLasdan <blackdm1024@gmail.com>
2024-12-23 00:03:10 +0700

    refactor: Migrate SkeletonLoader and KomikCard components to TypeScript, removing outdated JavaScript files and enhancing type safety

--- components/SkeletonLoader.tsx
+++ components/SkeletonLoader.tsx
@@ -1,4 +1,6 @@
-const SkeletonLoader = () => {
+import { FC } from 'react';
+
+const SkeletonLoader: FC = () => {
   return (
     <div className='relative flex h-[10vh] cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-700 p-4'></div>
   );
